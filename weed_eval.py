9f35ca44e50007bc54df90b177692e8994a0dde9
“skote05” <skote52004@gmail.com>
2025-11-26 10:44:57 +0530

    This gave amazing results 61 f1 score

--- weed_eval.py
+++ weed_eval.py
@@ -34,8 +34,8 @@ torch.set_num_threads(4)
 # Updated based on visual analysis success
 RESIZE_TO = (800, 800)
 DETECTION_SCALES = [1.0, 1.25, 1.5, 2.0]  # Reduced from [1.0, 1.25, 1.5, 2.0] for speed
-CONFIDENCE_THRESHOLD = 0.7    # Increased from 0.5 based on visual results
-NMS_THRESHOLD = 0.2
+CONFIDENCE_THRESHOLD = 0.8    # Increased from 0.5 based on visual results
+NMS_THRESHOLD = 0.15
 
 # Weed segmentation thresholds (from visual code)
 EXG_THRESHOLD = 20
